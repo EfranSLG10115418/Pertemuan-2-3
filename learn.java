c3f91d0b82e0461a850de557d3a169920d2d5863
Efran Sucipto Lumban Gaol <efransuciptolumbangaol@gmail.com>
2017-03-01 17:23:46 +0700

    Add files via upload

---
+++ learn.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+public class learn {
+
+    public static void main(String[] args) {
+        String nama;
+        Scanner namaVariabel = new Scanner(System.in);
+        nama = namaVariabel.nextLine();
+       
+        }
+    
+}
